95d11e88c054eeac293ccc621eeb91b5e0029138
MacStudent <macstudent@126-iMac09.local>
2019-03-09 12:08:40 -0500

    main login page (Harsh)

---
+++ Group6_W2019_MAD3115_FP/Model/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  Group6_W2019_MAD3115_FP
+//
+//  Created by MacStudent on 2019-03-09.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import Foundation
+
+
+class User
+{
+    
+    
+    
+    init() {
+        
+    }
+    
+    var userId : String!
+    var password : String!
+    
+    init( password : String,  userId : String)   {
+        self.userId = userId
+        self.password = password
+    }
+    
+    
+    
+}
