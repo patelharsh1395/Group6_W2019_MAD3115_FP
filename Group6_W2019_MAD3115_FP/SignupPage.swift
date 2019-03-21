c6382f21742ecedb12eb810055caa8942682a960
Harsh <harsh1395@Harshs-MacBook-Pro.local>
2019-03-21 19:16:51 -0400

    .....

---
+++ Group6_W2019_MAD3115_FP/SignupPage.swift
@@ -0,0 +1,30 @@
+//
+//  SignupPage.swift
+//  Group6_W2019_MAD3115_FP
+//
+//  Created by Harsh on 2019-03-21.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+import UIKit
+
+class SignupPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
